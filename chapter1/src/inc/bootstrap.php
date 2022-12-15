18ffe390c744918d6e0f417a34d6175de345c51c
Florian Gubler <gubler.florian@gmx.net>
2022-12-15 08:54:00 +0100

    Create API

---
+++ chapter1/src/inc/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+define("PROJECT_ROOT_PATH", __DIR__ . "/../");
+
+// include main configuration file
+require_once PROJECT_ROOT_PATH . "/inc/config.php";
+
+// include the base controller file
+require_once PROJECT_ROOT_PATH . "/controller/basecontroller.class.php";
+
+// include the use model file
+require_once PROJECT_ROOT_PATH . "/model/usermodel.class.php";
